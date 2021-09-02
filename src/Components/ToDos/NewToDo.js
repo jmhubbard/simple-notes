5c83aa2f881ced66bf1c03fd7ce838bfaeab4cf8
Jason Hubbard <jasonhubb@gmail.com>
2021-09-01 20:39:49 -0700

    adds a Ref to the todoInput and prints to console

--- src/Components/ToDos/NewToDo.js
+++ src/Components/ToDos/NewToDo.js
@@ -1,13 +1,19 @@
-
+import { useRef } from "react";
 
 const NewToDo = () => {
+    const toDoInputRef = useRef();
+    
+
+
     const submitHandler = (event) => {
         event.preventDefault();
+        const newToDo = toDoInputRef.current.value
+        console.log(newToDo);
     }
     return (
         <form onSubmit={submitHandler}>
             <label htmlFor="new todo">Enter a new todo</label>
-            <input type="text" id="newToDo"/>
+            <input type="text" id="newToDo" ref={toDoInputRef}/>
         </form>
     )
 };
