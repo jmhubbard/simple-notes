e676c07e9c7b8b0c81e67ca99eea1063079b479a
Jason Hubbard <jasonhubb@gmail.com>
2021-09-01 22:41:22 -0700

    Creates a newTask inside the submitHandler and then calls the onAddNewToDo function to pass data back to parent

--- src/Components/ToDos/NewToDo.js
+++ src/Components/ToDos/NewToDo.js
@@ -1,21 +1,25 @@
 import { useRef } from "react";
 
-const NewToDo = () => {
-    const toDoInputRef = useRef();
-    
+const NewToDo = (props) => {
+  const toDoInputRef = useRef();
 
-
-    const submitHandler = (event) => {
-        event.preventDefault();
-        const newToDo = toDoInputRef.current.value
-        console.log(newToDo);
-    }
-    return (
-        <form onSubmit={submitHandler}>
-            <label htmlFor="new todo">Enter a new todo</label>
-            <input type="text" id="newToDo" ref={toDoInputRef}/>
-        </form>
-    )
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newToDo = toDoInputRef.current.value;
+    const newTask = {
+      id: Math.random().toString(),
+      task: newToDo,
+      completed: false,
+    };
+    props.onAddNewToDo(newTask);
+    toDoInputRef.current.value = "";
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="new todo">New ToDo</label>
+      <input type="text" id="newToDo" ref={toDoInputRef}/>
+    </form>
+  );
 };
 
 export default NewToDo;
