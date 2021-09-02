04a9dbd6336dd6371b7c25a0c45b25d2c0ffbfd5
Jason Hubbard <jasonhubb@gmail.com>
2021-09-01 20:33:22 -0700

    Creates DUMMY_DATA and maps a ToDoItem to each item

--- src/Components/ToDos/ToDoList.js
+++ src/Components/ToDos/ToDoList.js
@@ -1,10 +1,30 @@
+import { Fragment } from "react";
+
 import ToDoItem from "../ToDoItem";
+import NewToDo from "./NewToDo";
+
+const DUMMY_DATA = [
+  { id: 1, task: "Learn React", completed: false },
+  { id: 2, task: "Buy Groceries", completed: false },
+  { id: 3, task: "Create to do app", completed: false },
+];
 
 const ToDoList = () => {
+  const allToDoItems = DUMMY_DATA.map((todo) => {
+    return (
+      <ToDoItem
+        id={todo.id}
+        key={todo.id}
+        task={todo.task}
+        completed={todo.completed}
+      />
+    );
+  });
   return (
-    <ul>
-      <ToDoItem />
-    </ul>
+    <Fragment>
+      <ul>{allToDoItems}</ul>
+      <NewToDo />
+    </Fragment>
   );
 };
 
