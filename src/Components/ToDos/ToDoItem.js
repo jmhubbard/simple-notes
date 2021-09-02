92042568734a5e67cecebd0e08624a5a6270713f
Jason Hubbard <jasonhubb@gmail.com>
2021-09-01 23:39:56 -0700

    Creates deleteItemHandler that will call props.onDeleteItem when the delete button image is clicked

--- src/Components/ToDos/ToDoItem.js
+++ src/Components/ToDos/ToDoItem.js
@@ -1,12 +1,16 @@
-import classes from "./ToDoItem.modules.css"
+import classes from "./ToDoItem.modules.css";
 
-import deleteImage from "../../assets/images/delete.png"
+import deleteImage from "../../assets/images/delete.png";
 
 const ToDoItem = (props) => {
+  const deleteItemHandler = () => {
+    props.onDeleteItem(props.id);
+  };
+
   return (
-    <div class="toDoItem">
+    <div className="toDoItem">
       <p>{props.task}</p>
-      <img src={deleteImage} alt="Delete Button"/>
+      <img src={deleteImage} alt="Delete Button" onClick={deleteItemHandler} />
     </div>
   );
 };
