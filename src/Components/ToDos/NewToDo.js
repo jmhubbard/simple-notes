b8a2795a3dc1af0674dcc2d612cef6204f77b372
Jason Hubbard <jasonhubb@gmail.com>
2021-09-06 16:07:48 -0700

    Creates a new div for creating a border around the form

--- src/Components/ToDos/NewToDo.js
+++ src/Components/ToDos/NewToDo.js
@@ -21,25 +21,27 @@ const NewToDo = (props) => {
 
   return (
     <div className={classes.newToDo}>
-      <form onSubmit={submitHandler}>
-        <div className={classes.newToDoText}>
-          <input
-            type="text"
-            id="newToDoInput"
-            placeholder="Enter a new toDo"
-            ref={toDoInputRef}
-            className={classes.newToDo__text}
-          />
-        </div>
-        <div className={classes.addNewToDoButton}>
-          <input
-            type="image"
-            alt="Add new toDo button"
-            src={addToDoButton}
-            className={classes.addToDoButton}
-          />
-        </div>
-      </form>
+      <div className={classes.newToDo__box}>
+        <form onSubmit={submitHandler}>
+          <div className={classes.newToDoText}>
+            <input
+              type="text"
+              id="newToDoInput"
+              placeholder="Enter a new toDo"
+              ref={toDoInputRef}
+              className={classes.newToDo__text}
+            />
+          </div>
+          <div className={classes.addNewToDoButton}>
+            <input
+              type="image"
+              alt="Add new toDo button"
+              src={addToDoButton}
+              className={classes.addToDoButton}
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
