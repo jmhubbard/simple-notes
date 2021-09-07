83dc6f486c7b935dc8a5db0f840ef1382d15a077
Jason Hubbard <jasonhubb@gmail.com>
2021-09-06 20:22:07 -0700

    adds validation to new task input

--- src/Components/ToDos/NewToDo.js
+++ src/Components/ToDos/NewToDo.js
@@ -10,38 +10,43 @@ const NewToDo = (props) => {
   const submitHandler = (event) => {
     event.preventDefault();
     const newToDo = toDoInputRef.current.value;
-    const newTask = {
-      id: Math.random().toString(),
-      task: newToDo,
-      completed: false,
-    };
-    props.onAddNewToDo(newTask);
+    const cleanedNewToDo = newToDo.trim();
+
+    if (cleanedNewToDo.length > 0) {
+      const newTask = {
+        id: Math.random().toString(),
+        task: cleanedNewToDo,
+        completed: false,
+      };
+      props.onAddNewToDo(newTask);
+    }
     toDoInputRef.current.value = "";
+
   };
 
   return (
     // <div className={classes.newToDo}>
-      <div className={classes.newToDoItem}>
-        <form onSubmit={submitHandler}>
-          <div className={classes.newToDoItem__Text}>
-            <input
-              type="text"
-              id="newToDoInput"
-              placeholder="Enter a new to-do"
-              ref={toDoInputRef}
-              // className={classes.newToDo__text}
-            />
-          </div>
-          <div className={classes.newToDoItem__AddButton}>
-            <input
-              type="image"
-              alt="Add new toDo button"
-              src={addToDoButton}
-              // className={classes.addToDoButton}
-            />
-          </div>
-        </form>
-      </div>
+    <div className={classes.newToDoItem}>
+      <form onSubmit={submitHandler}>
+        <div className={classes.newToDoItem__Text}>
+          <input
+            type="text"
+            id="newToDoInput"
+            placeholder="Enter a new to-do"
+            ref={toDoInputRef}
+            // className={classes.newToDo__text}
+          />
+        </div>
+        <div className={classes.newToDoItem__AddButton}>
+          <input
+            type="image"
+            alt="Add new toDo button"
+            src={addToDoButton}
+            // className={classes.addToDoButton}
+          />
+        </div>
+      </form>
+    </div>
     // </div>
   );
 };
