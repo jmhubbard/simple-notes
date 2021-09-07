51b6a992f0af91d1cac64615457d08326077196d
Jason Hubbard <jasonhubb@gmail.com>
2021-09-06 20:08:56 -0700

    Changes Header text to Simple To-Do's

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import classes from "./Header.module.css";
 const Header = () => {
   return (
     <div className={classes.header}>
-      <h1>Simple Notes</h1>
+      <h1>Simple To-Do's</h1>
     </div>
   );
 };
